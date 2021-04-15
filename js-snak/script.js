9b30d4b0631157aaa2de82986be2659fdafc9eb2
Giulio_Pa <giuliop93@hotmail.it>
2021-04-15 18:10:37 +0200

    wip

---
+++ js-snak/script.js
@@ -0,0 +1,82 @@
+// l'utente inserisce due numeri in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.// 
+
+var primoNumero = parseInt(prompt("inserisci un numero"));
+var secondoNumero = prompt("inserisci un altro numero");
+
+if (primoNumero > secondoNumero) {
+    console.log(primoNumero)
+}else if(secondoNumero > primoNumero) {
+    console.log(secondoNumero)
+} else {
+    console.log("i due numeri sono uguali")
+}
+
+// 7 5 --> 7
+// 5 7 --> 7
+// 5 5 --> i due numeri sono uguali
+
+
+// l'utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+var = primaPaorla = prompt("inserisci una parola");
+var = secondaParola = prompt ("inserisci un'altra parola");
+
+if(maggioreDi(primaParola, secondaParola) == -1){
+    console.log()
+}
+if(maggiore)
+
+if(PrimaParola.lenght > SecondaParola.lenght) {
+    console.log(primaParola, secondaParola)
+} else if(SecondaParola.lenght > PrimaParola.lenght) {
+    console.log(secondaParola, primaParola)
+} else {
+    console.log("le parole hanno la stessa lunghezza")
+}
+
+console.log("la parola più lunga è: ilMaggiore")
+console.log("la parola più corta e")
+
+// 'ciao' 'aiuto' -> la parola più lunga e\aiuto
+// 'aiuto' 'ciao' ->  la parola più lunga e
+
+function maggioreDi (parola1, parola2) {
+
+    if(return parola1.lenght > parola2.lenght
+//     if(parola1.lenght > parola2.lenght) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }/** */
+
+
+// crea un array vuoto./**
+
+var numeri = []
+
+// chiedi per sei volte all'utente di inserire un numero.
+
+var numeroInserito = parseInt(prompt("inserisci un numero"));
+
+for (var i = 0; i < 6; i++ ) {
+    var input = parseInt(prompt("inserisci numeri"))
+
+    if(isNaN(input)){
+        var input = parseInt(prompt("inserisci numeri"))
+    }
+
+    if(iinput % 2 != 0){
+        array.push(inserisciNumeri)
+    }
+}
+
+// 7 9 8 2 5 10 -->[7,9,5]
+//ciao 4 3 5 4 6 --> [3,3,5]
+// 'ciao' 'ciao1 1 2 3 5 4 6 --> [1,3,5]
+
+function preendiNummero() {
+    do {
+        var input = parseInt(prompt('inserisci'))
+    }
+}
